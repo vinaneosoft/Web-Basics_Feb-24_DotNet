f82c66c64adbd84866f5b9a7b1d4d3860f18f8be
kokanevina <kokane.vina@gmail.com>
2024-02-20 15:46:44 +0530

    commit

--- JAVASCRIPT/JavascriptBasics/Functions.js
+++ JAVASCRIPT/JavascriptBasics/Functions.js
@@ -1,5 +1,5 @@
 // basic functions : named functions
-// annonymous functions : without name
+
 // arrow functions : compact declaration
 
 // non-parameter, parameter functions
@@ -12,8 +12,40 @@ demo1();
 
 function  add(num1,num2){
     let res=num1+num2;
+    console.log(typeof num1);
+    console.log(typeof num2);
     return res;
 }
 let addition1=add(45,67);
 console.log(addition1);
 console.log(    add(450,679)      );
+console.log(    add("mahesh ","patil")      );
+
+// annonymous functions : without name
+let demo2=     function() {
+    // function body code
+    console.log("in annonymous function.....");
+}
+demo2();
+
+let refdemo=demo2; // pass the ref to another variable
+demo2=function(){
+    console.log("in another annonymous function.....");
+}
+demo2();
+demo2();
+demo2();
+
+refdemo();
+// if we do not want to change the references then declare const ref
+
+const demo3=function(){
+
+}
+
+const demo4=function(){
+
+}
+demo3=function(){
+    
+}
