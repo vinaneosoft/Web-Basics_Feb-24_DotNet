404bb42e3290b6105d6e6a6bc219362f7d40dfbd
kokanevina <kokane.vina@gmail.com>
2024-02-20 14:41:14 +0530

    commit

---
+++ JAVASCRIPT/JavascriptBasics/Variables.js
@@ -0,0 +1,30 @@
+/*
+// dynamically typed lang: javascript
+// var v1, let v2,, const v3
+// v1=34; v="Hello"; v=true  : allowed
+// data type is decided at run time number, string,boolean
+*/
+
+
+/*
+//statically typed lang : java, c, c++
+//int v  : v will accept only values compatible with int datatype
+// v=45 okay v="Hello" : error
+// type of variable is decided in prior b4 execution
+*/
+
+var companyName;
+console.log(typeof companyName);
+companyName="Neosoft";
+console.log(typeof companyName);
+companyName=8900099;
+console.log(typeof companyName);
+companyName=true;
+console.log(typeof companyName);
+
+let locationStatus;
+locationStatus=0;
+
+locationStatus="YES";
+
+locationStatus=true
