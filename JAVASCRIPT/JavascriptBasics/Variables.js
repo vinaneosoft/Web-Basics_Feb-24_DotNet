b32ada231a84ec254edc62cebfdbb17c9a6f556d
kokanevina <kokane.vina@gmail.com>
2024-02-20 15:06:49 +0530

    commit

--- JAVASCRIPT/JavascriptBasics/Variables.js
+++ JAVASCRIPT/JavascriptBasics/Variables.js
@@ -24,7 +24,40 @@ console.log(typeof companyName);
 
 let locationStatus;
 locationStatus=0;
-
+console.log(typeof locationStatus);
 locationStatus="YES";
-
+console.log(typeof locationStatus);
 locationStatus=true
+console.log(typeof locationStatus);
+// var type variable declared inside function always has functional scope
+function demo1(){
+    console.log("outside"+i);
+    for(var i=1;i<=5;i++){
+        console.log("inside: "+i);
+    }
+    console.log("outside"+i);
+}
+
+demo1();
+
+ /*let type variables have block scope where they are declared */
+function demo2(){
+    //console.log("outside"+i); // error
+    for(let i=1;i<=5;i++){
+        console.log("inside: "+i);
+    }
+    console.log("outside"+i); // error
+}
+
+console.log("DEMO2 called.......");
+demo2();
+
+/** var type variables can be redeclared in same scope */
+var employeeId=4878;
+var employeeId=8888;
+console.log(employeeId);
+
+/** let type variables can not be redeclared in same scope */
+let empName="Pooja";
+//let empName="Hari";
+
