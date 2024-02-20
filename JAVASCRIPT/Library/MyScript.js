1bdc3d1e372662550cf9ffb7eb269e2cd0e18fdb
kokanevina <kokane.vina@gmail.com>
2024-02-20 13:55:46 +0530

    commit

--- JAVASCRIPT/Library/MyScript.js
+++ JAVASCRIPT/Library/MyScript.js
@@ -1,3 +1,15 @@
 // do not put script tag
 /** */   
 document.write('<p>this is paragraph added via external script</p>');
+document.write("<span>hi</span>",'<b>hello</b>','<h4>welcome</h4>');
+document.write("Neosoft"); //error // exeuction stops here
+document.write("Rabale");
+document.write("Rabale");
+document.write("Rabale");
+document.write("Rabale");
+
+
+/* Interpretation : direct execution (error checking while exeucting the code)
+script gets interpreted by browser
+
+compilation : conversion (error checking), then execution*/
